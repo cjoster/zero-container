263b49d2ee3cab9aada1ed2c83e7eeef45cba206
CJ Oster <cjo@redhat.com>
2018-03-29 15:26:57 -0500

    Adds code comments

--- zero.s
+++ zero.s
@@ -1,7 +1,12 @@
+# Global Symbol _start
 .globl	_start
 
+# _start function
 _start:
-	movl $1, %eax
-	xorl %ebx, %ebx
-	int  $0x80
+	movl $1, %eax   # Syscall 1
+	xorl %ebx, %ebx # return value 0
+	int  $0x80      # Syscall
+
+        # No need to return because the exit
+        # syscall never returns
 
